42879f77a4c7835526845c3d97f72156c05af2df
MalikIsmaeel <m.gambour@hotmail.com>
2022-09-08 21:22:54 +0300

    changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,19 +60,7 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('active') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="cgeckbox" type="text" class="form-control @error('active') is-invalid @enderror" name="active" value="{{  1 ?? old('active') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
