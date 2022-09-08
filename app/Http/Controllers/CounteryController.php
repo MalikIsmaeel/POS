6f687af285607e6ac450522ef533d3c3839ed2d7
unknown <m.gambour@hotmail.com>
2022-09-08 10:57:27 +0300

    add countery first touch

---
+++ app/Http/Controllers/CounteryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\countery;
+use Illuminate\Http\Request;
+
+class CounteryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\countery  $countery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(countery $countery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\countery  $countery
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(countery $countery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\countery  $countery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, countery $countery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\countery  $countery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(countery $countery)
+    {
+        //
+    }
+}
---
+++ database/seeders/CounterySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CounterySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
