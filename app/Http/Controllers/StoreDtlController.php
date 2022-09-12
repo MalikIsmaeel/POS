fa4db7bacaef5345f370f758aea23616acb07ab2
MalikIsmaeel <m.gambour@hotmail.com>
2022-09-12 06:01:36 +0300

    add storage models

---
+++ app/Http/Controllers/StoreDtlController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\store_dtl;
+use Illuminate\Http\Request;
+
+class StoreDtlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\store_dtl  $store_dtl
+     * @return \Illuminate\Http\Response
+     */
+    public function show(store_dtl $store_dtl)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\store_dtl  $store_dtl
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(store_dtl $store_dtl)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\store_dtl  $store_dtl
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, store_dtl $store_dtl)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\store_dtl  $store_dtl
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(store_dtl $store_dtl)
+    {
+        //
+    }
+}
