60cbf67539e9b5d117bd5a349394432d7a9b39fe
unknown <m.gambour@hotmail.com>
2023-01-19 22:03:32 +0300

    qrcode

---
+++ app/Http/Controllers/CreditNoteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\credit_note;
+use Illuminate\Http\Request;
+
+class CreditNoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+}
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\credit_note  $credit_note
+     * @return \Illuminate\Http\Response
+     */
+    public function show(credit_note $credit_note)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\credit_note  $credit_note
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(credit_note $credit_note)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\credit_note  $credit_note
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, credit_note $credit_note)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\credit_note  $credit_note
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(credit_note $credit_note)
+    {
+        //
+    }
+}
--- resources/views/pdf-with-qr.blade.php
+++ resources/views/pdf-with-qr.blade.php
@@ -3,23 +3,20 @@
 <html>
 
 <head>
-    <title>pdf test</title>
+    <title>{{ $title }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 
 <body>
-
-    <h1>this is {{ $company_name }} with id {{$id}}</h1>
-
-    <p>{!! $par_code!!}</p>
-    <p>{{ $invoice_date }}</p>
-    <p>{{$total_amount}}</p>
-    <p>yes It worked</p>
+    <h1>{{ $title }}</h1>
+    <p>{{ $date }}</p>
+    <p>{!! $qr_image !!}</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </body>
-<script>
-    $(window).ready(function () {
-    window.print();
-    })
-</script>
 
 </html>
